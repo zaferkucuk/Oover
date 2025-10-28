bfbce22ae5cd99081befbc223b60210845084290
zaferkucuk <53651443+zaferkucuk@users.noreply.github.com>
2025-10-28 18:19:50 +0100

    feat: Add Django project __init__.py

---
+++ backend/oover_backend/__init__.py
@@ -0,0 +1,17 @@
+"""
+Oover Backend Django Project
+
+This package contains the main Django project configuration for the Oover
+sport prediction application.
+
+Author: Oover Development Team
+Date: October 2025
+"""
+
+# This will make sure the app is always imported when
+# Django starts so that shared_task will use this app.
+from .celery import app as celery_app
+
+__all__ = ('celery_app',)
+
+__version__ = '1.0.0'
